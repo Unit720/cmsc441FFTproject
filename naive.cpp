1289fae318edbe8d355d2964c4a0c11782331684
Unknown <dave.pan1@gmail.com>
2017-03-09 20:34:32 -0500

    Naive with Pink
    more stuffs

--- naive.cpp
+++ naive.cpp
@@ -2,9 +2,22 @@
 
 #include <iostream>
 #include <cmath>
+#include "PinkNoise.h"
+
+#define SIGNALENGTH 65535
 
 int main() {
+  PinkNoise noisegen;
+  double signal[SIGNALLENGTH];
+  double inreal[SIGNALLENGTH];
+  double inimag[SIGNALLENGTH];
+  double outreal[SIGNALLENGTH];
+  double outimag[SIGNALLENGTH];
   
+  for (int i = 0; i < SIGNALLENGTH; i++){
+    signal[i] = noisegen.tick();
+  }
+  naivedft();
 }
 
 void naivedft(double[] inreal, double[] inimag, double[] outreal, double[] outimag) {
@@ -13,7 +26,7 @@ void naivedft(double[] inreal, double[] inimag, double[] outreal, double[] outim
     double sumreal = 0;
     double sumimag = 0;
     for (int t = 0; t < n; t++) {  // For each input element
-      double angle = 2 * Math.PI * t * k / n;
+      double angle = 2 * M_PI * t * k / n;
       sumreal +=  inreal[t] * Math.cos(angle) + inimag[t] * Math.sin(angle);
       sumimag +=  inimag[t] * Math.cos(angle) - nreal[t] * Math.sin(angle);
     }
