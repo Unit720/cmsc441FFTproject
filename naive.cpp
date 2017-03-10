fc2aeef5753f0c54b162e016fd9dc635e02aa487
Unknown <dave.pan1@gmail.com>
2017-03-09 20:53:12 -0500

    syntax cleanup
    here's a little more

--- naive.cpp
+++ naive.cpp
@@ -4,8 +4,8 @@
 #include <cmath>
 #include "PinkNoise.h"
 
-#define SIGNALENGTH 65535
-
+#define SIGNALLENGTH 65535
+void naivedft(double[], double[], double[], double[]);
 int main() {
   PinkNoise noisegen;
   double signal[SIGNALLENGTH];
@@ -15,20 +15,28 @@ int main() {
   double outimag[SIGNALLENGTH];
   
   for (int i = 0; i < SIGNALLENGTH; i++){
-    signal[i] = noisegen.tick();
+    inreal[i] = noisegen.tick();
+    std::cout << inreal[i] << " ";
   }
-  naivedft();
+  std::cout << std::endl;
+  naivedft(signal, inimag, outreal, outimag);
+
+  for (int i = 0; i < SIGNALLENGTH; i++){
+    std::cout << outreal[i] << " ";
+  }
+  std::cout << std::endl;
+  
 }
 
-void naivedft(double[] inreal, double[] inimag, double[] outreal, double[] outimag) {
-  int n = inreal.length;
+void naivedft(double* inreal, double* inimag, double* outreal, double* outimag) {
+  int n = sizeof(inreal) / sizeof(double);
   for (int k = 0; k < n; k++) {  // For each output element
     double sumreal = 0;
     double sumimag = 0;
     for (int t = 0; t < n; t++) {  // For each input element
       double angle = 2 * M_PI * t * k / n;
-      sumreal +=  inreal[t] * Math.cos(angle) + inimag[t] * Math.sin(angle);
-      sumimag +=  inimag[t] * Math.cos(angle) - nreal[t] * Math.sin(angle);
+      sumreal +=  inreal[t] * cos(angle) + inimag[t] * sin(angle);
+      sumimag +=  inimag[t] * cos(angle) - inreal[t] * sin(angle);
     }
     outreal[k] = sumreal;
     outimag[k] = sumimag;
